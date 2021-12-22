3b794351a688fc8691955da11cf261eea02374b9
ranjan <86014308+r1a2n3@users.noreply.github.com>
2021-12-22 10:42:51 +0530

    firebase2

--- src/components/firebase/db.js
+++ src/components/firebase/db.js
@@ -1,1 +1,16 @@
 // hello
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6lV7YOjgy3u_EdFaPPZp2lqeFFYDB-oo",
+  authDomain: "healthcalculators-45fea.firebaseapp.com",
+  databaseURL: "https://healthcalculators-45fea-default-rtdb.firebaseio.com",
+  projectId: "healthcalculators-45fea",
+  storageBucket: "healthcalculators-45fea.appspot.com",
+  messagingSenderId: "165126525528",
+  appId: "1:165126525528:web:1a27a69e6ffd33f0caf02d",
+  measurementId: "G-7M6J6VVN1F",
+};
+
+export const db = initializeApp(firebaseConfig).firestore();
--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import "./registerServiceWorker";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { firestorePlugin } from "vuefire";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.use(firestorePlugin);
 Vue.config.productionTip = false;
